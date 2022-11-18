4528205dd808b12c294bbe08fb61e735360ebaac
Tobiloba.Ekisola <Tobiloba.Ekisola@ISW2-220804-874>
2022-11-18 21:41:39 +0100

    Add project files.

---
+++ ParkingR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ParkingR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
